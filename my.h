98154d1fe0d35edd2b7690d81603e9b77bd6341d
Zero <bruno.lauranne1@gmail.com>
2020-02-01 19:00:32 +0100

    Update my.h
    Clean file

--- my.h
+++ my.h
@@ -1,14 +1,9 @@
 #ifndef MY_H_
 # define MY_H_
 
-/* #define NB_SAMPLES 3 */
-/* #define NB_TRACES 10 */
 #define NB_SAMPLES 980
 #define NB_TRACES 2000
 #define OFFSET_RES 31
-
-
-
 
 void get_traces(double **traces, int**input);
 double **alloc_trace();
